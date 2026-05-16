52a2618b2d322ce5dc44bf5f1ab3d4ef33c552a4
Makna Kreatif Team <retriyuselli@maknakreatif.id>
2026-05-16 16:10:43 +0700

    Update

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Models\Role;
 
@@ -34,6 +35,8 @@ class RegisterController extends Controller
 
         Role::findOrCreate('pengunjung', 'web');
         $user->assignRole('pengunjung');
+
+        Auth::login($user);
 
         $user->sendEmailVerificationNotification();
 
