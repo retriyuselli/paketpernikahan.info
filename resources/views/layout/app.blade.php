f409b8dd0b40980556f7de76c02657396b9dcfde
Makna Kreatif Team <retriyuselli@maknakreatif.id>
2026-05-20 00:07:52 +0700

    Update

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -13,7 +13,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
         <style>
-            body { padding-bottom: env(safe-area-inset-bottom); }
+            body { padding-top: calc(60px + env(safe-area-inset-top)); padding-bottom: env(safe-area-inset-bottom); }
             .safe-area-header { padding-top: env(safe-area-inset-top); }
         </style>
         <meta name="csrf-token" content="{{ csrf_token() }}">
