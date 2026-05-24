e6bf8c799849b9921e134e9e52861100bb9c917f
Makna Kreatif Team <retriyuselli@maknakreatif.id>
2026-05-24 22:31:45 +0700

    Update

--- routes/console.php
+++ routes/console.php
@@ -9,4 +9,6 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 // Refresh Instagram token setiap 30 hari (token berlaku 60 hari)
-Schedule::command('instagram:refresh-token')->monthlyOn(1, '02:00');
+Schedule::command('instagram:refresh-token')
+    ->monthlyOn(1, '02:00')
+    ->appendOutputTo(storage_path('logs/instagram-token.log'));
